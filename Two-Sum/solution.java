f3b2870bcef3444fcac186243145ee0df123e88c
Berk SAĞDIÇ <129013056+sagdicberk@users.noreply.github.com>
2024-09-11 00:30:34 +0300

    Update solution.java

--- Two-Sum/solution.java
+++ Two-Sum/solution.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] listOfIndex = new int[2];
+        
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+               int optionalTarget = nums[i] + nums[j];
+               if(target == optionalTarget){
+                 if(i != j){
+                    listOfIndex[0] = i;
+                    listOfIndex[1] = j;
+                    return listOfIndex;
+                 }
+                 
+               }
+            }
+        }
+        return listOfIndex;
+    }
+}
