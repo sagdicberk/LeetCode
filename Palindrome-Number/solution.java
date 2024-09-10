af684cc51c19b720a99faaf9c0eddd49e64d4b93
Berk SAĞDIÇ <129013056+sagdicberk@users.noreply.github.com>
2024-09-11 01:07:20 +0300

    Create solution.java

---
+++ Palindrome-Number/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int number = x;
+        int reverse = 0;
+        
+        if(number < 0){
+            return false;
+        }
+
+
+        while(x != 0){
+            int digit = x % 10;
+            reverse = reverse*10 + digit;
+            x = x / 10;
+            System.out.println(reverse);
+
+            if(number == reverse){
+                return true;
+            }
+        }
+       
+
+        return false;
+    }
+}
